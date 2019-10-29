60716f52f03ccc570e209a87119d638966cc33bc
Andrew Chang <jychang109@gmail.com>
2019-10-29 23:08:06 +0800

    update to while i>0

--- password-retry.py
+++ password-retry.py
@@ -1,12 +1,10 @@
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
 	pw = input('請輸入密碼(最多三次)：')
 	if pw == password:
 		print('登入成功')
 		break
 	else:
 		i = i - 1
-		if i == 0:
-			break
 		print('密碼錯誤！還有 ', i, ' 次機會！')
